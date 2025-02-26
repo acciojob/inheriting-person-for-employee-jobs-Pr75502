bb0c9a2429c1a8e0bca32d39ff3fdbe5f7fcac73
Pr75502 <pp1614582@gmail.com>
2025-02-26 21:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Person(name, age) {
 	
 }
 Person.prototype.greet=function(){
-		return console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+		return console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
 	}
 
 function Employee(name, age, jobTitle) {
@@ -19,8 +19,9 @@ function Employee(name, age, jobTitle) {
 Employee.prototype=Object.create(Person.prototype)
 Employee.prototype.constructor=Employee
 Employee.prototype.jobGreet=function(){
-			return console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+		return console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
 		}
+
 
 // Do not change code below this line
 window.Person = Person;
